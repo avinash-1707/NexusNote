2b800d48ddb32e52b26150f70aaa884c10f3f0be
Avinash <avinashnarwariya5@gmail.com>
2026-05-11 13:43:44 +0530

    added production url to cors config

--- apps/api/main.py
+++ apps/api/main.py
@@ -8,7 +8,7 @@ app = FastAPI(title="NexusNote API", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://nexusnote.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
